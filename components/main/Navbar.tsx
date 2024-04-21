d0ffca46e69b084c1fd2aa39379f1a84bfabcc06
fadaeixlii <mohammad4252@gmail.com>
2024-04-21 13:56:32 +0330

    Add navbar

---
+++ components/main/Navbar.tsx
@@ -0,0 +1,54 @@
+import { Socials } from "@/constants";
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2a0e61]/50  bg-[#03001417] backdrop-blur-md z-50 px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+        <a href="#about-me" className="h-auto w-auto flex items-center">
+          <Image
+            src={"/NavLogo.png"}
+            height={70}
+            width={70}
+            className="cursor-pointer hover:animate-spin"
+            alt="logo"
+          />
+          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
+            WebChain DEV
+          </span>
+        </a>
+        <div
+          className="w-[500px]
+        h-full flex items-center justify-between md:mr-20
+        "
+        >
+          <div className="flex items-center w-full h-auto justify-between border border-[#7042f862] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+            <a href="#about-me" className="cursor-pointer">
+              About me
+            </a>
+            <a href="#skills" className="cursor-pointer">
+              Skills
+            </a>
+            <a href="#projects" className="cursor-pointer">
+              Projects
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-row gap-5 ">
+          {Socials.map((social) => (
+            <Image
+              src={social.src}
+              alt={social.name}
+              key={social.name}
+              width={24}
+              height={24}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,9 +2,14 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromLeft, slideInFromTop } from "@/utils/motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+
 const HeroContent = () => {
   return (
     <motion.div
@@ -17,46 +22,52 @@ const HeroContent = () => {
           variants={slideInFromTop}
           className="welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] size-5" />
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="welcome-text text-[13px]">
-            FullStack developer portfolio
+            Fullstack Developer Portfolio
           </h1>
         </motion.div>
+
         <motion.div
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto  h-auto"
           variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
-          <span className="">Providing </span>
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-            the best
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+            project exprience
           </span>
-          <span className=""> Project experience </span>
         </motion.div>
+
         <motion.p
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a FullStack software engineer with experience in Website,
-          Mobile, and software development. check out my projects and skills.
+          I&apos;m a Full Stack Software Engineer with experience in Website,
+          Mobile, and Software development. Check out my projects and skills.
         </motion.p>
         <motion.a
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
           variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-          Learn More
+          Learn More!
         </motion.a>
-        <motion.div
-          variants={slideInFromLeft(0.8)}
-          className="w-full h-full flex justify-center items-center"
-        >
-          <Image
-            src={"/mainIconsdark.svg"}
-            height={650}
-            width={650}
-            alt="main icon"
-          />
-        </motion.div>
       </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
     </motion.div>
   );
 };
